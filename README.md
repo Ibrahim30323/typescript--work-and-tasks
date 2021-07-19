87d652c423b0c106052b2954988da143923b5b8b
Ibrahim30323 <103577332@student.swin.edu.au>
2021-07-19 10:14:59 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-mj8wav
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-mj8wav)
---
+++ index.ts
@@ -0,0 +1,86 @@
+// Import stylesheets
+//import './style.css';
+
+// Write TypeScript code!
+//const appDiv: HTMLElement = document.getElementById('app');
+//appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+
+//var planet: string = "Earth";
+//var year: number = 2021;
+//var isLockdown: boolean = true;
+//var food: any = "noodle";
+
+// const colour = "green";
+// colour = "red";
+
+const appDiv: HTMLElement = document.getElementById('app');
+
+const output: HTMLElement = document.getElementById('output');
+
+
+const output: HTMLElement = douc
+button.addEventListener('click', fizzbuzz);
+
+function fizzbuzz() {
+   // count = 100
+   
+   for (let i = 1; i <= 100; i++) {
+
+     if (i % 3 == 0 && i % 5 == 0) {
+        console.log('fizzbuzz', i);
+     } else if (i % 3 == 0) {
+        console.log('fizz', i);  
+     } else if (i % 5 == 0) {
+        console.log('buzz', i);
+     } 
+      
+   }
+   
+}
+
+fizzbuzz();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
