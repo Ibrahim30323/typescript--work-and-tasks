eb84649e8cbaf2aebd235ccde3e34ac66c9cd61f
Ibrahim30323 <103577332@student.swin.edu.au>
2021-07-19 12:22:41 +1000

    2 files modified

--- index.ts
+++ index.ts
@@ -13,25 +13,25 @@
 // const colour = "green";
 // colour = "red";
 
-const appDiv: HTMLElement = document.getElementById('app');
+/*const output: HTMLElement = document.getElementById('output');
 
-const output: HTMLElement = document.getElementById('output');
+const button: HTMLElement = document.getElementById('custom-btn');
 
-
-const output: HTMLElement = douc
 button.addEventListener('click', fizzbuzz);
 
 function fizzbuzz() {
-   // count = 100
+   const startNumInput: HTMLInputElement = <HTMLInputElement>(document.getElementById('start-num')
+   );
+   const startNumVal: number = Number(startNumInput.value);
    
-   for (let i = 1; i <= 100; i++) {
+   for (let i = startNumVal; i <= 100; i++) {
 
      if (i % 3 == 0 && i % 5 == 0) {
-        console.log('fizzbuzz', i);
+        output.innerHTML += '<p>fizzbuzz ' + i + '</p>'
      } else if (i % 3 == 0) {
-        console.log('fizz', i);  
+        output.innerHTML += '<p>fizz' + i + '</p>'  
      } else if (i % 5 == 0) {
-        console.log('buzz', i);
+        output.innerHTML += '<p>buzz' + i + '</p>'
      } 
       
    }
@@ -39,6 +39,91 @@ function fizzbuzz() {
 }
 
 fizzbuzz();
+
+*/
+
+class Student {
+   firstName: string;
+   lastName: string;
+   id: number;
+   age: number;
+
+   contructor(
+      _firstName: string, 
+      _lastName: string, 
+      _id: number,
+      _age: number
+   ) {
+      this.firstName = _firstName;
+      this.lastName = _lastName;
+      this.id = _id;
+      this.age = _age;
+   }
+
+   getFullName() {
+      return `$(this.firstName) ${this.lastName}`;
+   }
+}
+
+const student1: Student = new Student('Jane', 'Smith', 123, 25);
+
+console.log(student1.firstName);
+console.log(student1.lastName);
+console.log(student1.getFullName());
+
+class Car {
+   speed: number;
+   rego: string;
+   maxSpeed: number = 120;
+
+   constructor(_speed: number, _rego: string) {
+      this.speed = _speed;
+      this.rego = _rego;
+   }
+
+   increaseSpeed(spd: number): void {
+      this.speed += spd;
+      
+      if (this.speed > this.maxSpeed) {
+          this.speed = this.maxSpeed;
+      }
+   }
+}
+
+let innerHtml: string = '';
+
+const selectDropdown: HTMLElement = document.getElementById('sports');
+const sports: string[] = ['soccer', 'basketball', 'baseball'];
+
+for (let i = 0; i < sports.length; i++) {
+   innerHtml += `<option value=${sports[i]}>sport: ${sports[i]} </option>`;
+}
+
+selectDropdown.innerHTML = innerHtml;
+
+
+
+
+
+const sports: string[] = ['soccer', 'basketball', 'baseball'];
+const sports2: Array<string> = ['soccer', 'basketball', 'baseball'];
+console.log(sports);
+console.log(sports2);
+
+//const carl: Car = new Car(10, "123abc");
+//carl.increaseSpeed(20);
+//console.log(carl);
+
+
+class Planet {
+   name: string;
+   numOfMoons: number;
+
+   constructor(_name: string, _numOfMoons: number) {
+      this.name = _name;
+      this.numOfMoons = _numOfMoons;
+   }
+}
 
 
 
